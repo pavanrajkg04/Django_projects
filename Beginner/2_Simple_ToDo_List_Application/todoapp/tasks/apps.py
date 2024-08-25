93be6ed18f6dcc07c87c3770efbc2e8c30260d65
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-08-25 14:07:27 +0000

    a

--- Beginner/2_Simple_ToDo_List_Application/todoapp/tasks/apps.py
+++ Beginner/2_Simple_ToDo_List_Application/todoapp/tasks/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TodolistappConfig(AppConfig):
+class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todolistapp'
+    name = 'tasks'
--- Beginner/2_Simple_ToDo_List_Application/todoapp/todolistapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path('', views.home, name='Home-page'),
-    path('login/', views.login, name='login-page'),
-    path('register/', views.register, name='register-page'),
-]
