4dbaa045282ba625397c85f99d20cfd92bab83be
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-08-27 05:06:20 +0000

    a

--- Beginner/2_Simple_ToDo_List_Application/todoapp/tasks/views.py
+++ Beginner/2_Simple_ToDo_List_Application/todoapp/tasks/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .models import Task
+from . forms import TaskForm
 
 def home(request):
     tasks = Task.objects.all()
+    form = TaskForm()
 
-    context = {'tasks' : tasks }
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        
+        return redirect('/')
+
+    context = {'tasks' : tasks , 'TaskForms': form}
 
     return render(request, 'tasks.html', context)
